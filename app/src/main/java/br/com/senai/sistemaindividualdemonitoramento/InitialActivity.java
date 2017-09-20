6320cc10b808572056803e5116db7924b8c07109
Kyro23 <cassiano.jr.2009@hotmail.com>
2017-09-20 10:53:48 -0300

    Initial commit

---
+++ app/src/main/java/br/com/senai/sistemaindividualdemonitoramento/InitialActivity.java
@@ -0,0 +1,15 @@
+package br.com.senai.sistemaindividualdemonitoramento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+
+    }
+}
