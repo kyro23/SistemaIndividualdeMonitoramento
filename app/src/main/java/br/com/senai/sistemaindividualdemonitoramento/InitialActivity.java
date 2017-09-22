a806afba82d0f78426292b7aeafbe1caa91db02b
Kyro23 <cassiano.jr.2009@hotmail.com>
2017-09-21 21:33:45 -0300

    Changes of the principal screen

--- app/src/main/java/br/com/senai/sistemaindividualdemonitoramento/InitialActivity.java
+++ app/src/main/java/br/com/senai/sistemaindividualdemonitoramento/InitialActivity.java
@@ -2,6 +2,8 @@ package br.com.senai.sistemaindividualdemonitoramento;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class InitialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
+        String[] defaultSpnText = {"Dobra", "Cola", "Outra coisa sei la", "Não sou da grafica kkkjj"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, defaultSpnText);
 
+        Spinner spn = (Spinner) findViewById(R.id.spn_activity);
+        spn.setAdapter(adapter);
     }
 }
